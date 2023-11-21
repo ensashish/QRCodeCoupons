ef751e264501b5a13c604952ddd67de15008457e
ensashish <146351732+ensashish@users.noreply.github.com>
2023-11-21 17:14:58 +0530

    Update dbConnect.js

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,17 +1,1 @@
-import mongoose from "mongoose";
 
-const dbConnection = async ()=>{
-   await mongoose.connect(process.env.MONGODB_URL, {}).then(()=>{
-        console.log("---Database connected successfully---");
-    }).catch((error)=>{
-        console.log("Database Error : "+error);
-    });
-    // try {
-    //     mongoose.connect("mongodb://localhost:27017/ecommerce");
-    //     console.log("Database connected successfully");
-    // } catch (error) {
-    //     console.log("Database Error : "+error);
-    // }
-}
-
-export default dbConnection;
